24b3e51ddf2e8ba53749e00aa4420fed8e783a7b
Samuel Marcelino Tindaon <samuelmarcelinotindaon@gmail.com>
2026-03-10 00:37:08 +0700

    [REFACTOR] separate payment data integrity checks to methods

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -15,40 +15,53 @@ public class Payment {
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
         this.id = id;
-
-        String[] validMethods = {"Voucher Code", "Payment by Bank Transfer"};
-        if (Arrays.asList(validMethods).contains(method)) {
-            this.method = method;
-        }
-        else {
-            throw new IllegalArgumentException();
-        }
-
-        if (PaymentStatus.contains(status)) {
-            this.status = status;
-        }
-        else {
-            throw new IllegalArgumentException();
-        }
-
-        if (method == "Voucher Code") {
-            if (!paymentData.get("voucherCode").contains("ESHOP") ||
-                    paymentData.get("voucherCode").length()!=16 ||
-                    paymentData.get("voucherCode").replaceAll("\\D","").length()!=8) {
-                this.status = "REJECTED";
-            }
-        }
-        else if (method == "Payment by Bank Transfer") {
-            if (paymentData.get("bankName")=="" ||
-                    paymentData.get("bankName")==null ||
-                    paymentData.get("referenceCode")=="" ||
-                    paymentData.get("referenceCode")==null) {
-                this.status = "REJECTED";
-            }
-        }
-
+        this.method = verifyMethod(method);
+        this.status = verifyStatus(status);
         this.paymentData = paymentData;
+        verifyPaymentData(paymentData);
     }
+
+    public String verifyMethod(String method) {
+        String[] validMethods = {"Voucher Code", "Payment by Bank Transfer"};
+        if (!Arrays.asList(validMethods).contains(method)) {
+            throw new IllegalArgumentException();
+        }
+        return method;
+    }
+
+    public String verifyStatus(String status) {
+        if (!PaymentStatus.contains(status)) {
+            throw new IllegalArgumentException();
+        }
+        return status;
+    }
+
+    public void verifyPaymentData(Map<String, String> paymentData) {
+        if (this.method == "Voucher Code") {
+            verifyVoucher(paymentData.get("voucherCode"));
+        }
+        else if (this.method == "Payment by Bank Transfer") {
+            verifyBank(paymentData.get("bankName"), paymentData.get("referenceCode"));
+        }
+    }
+
+    public void verifyVoucher(String voucherCode) {
+        if (!voucherCode.contains("ESHOP") ||
+                voucherCode.length()!=16 ||
+                voucherCode.replaceAll("\\D","").length()!=8) {
+            this.status = "REJECTED";
+        }
+    }
+
+    public void verifyBank(String bankName, String referenceCode) {
+        if (bankName=="" ||
+                bankName==null ||
+                referenceCode=="" ||
+                referenceCode==null) {
+            this.status = "REJECTED";
+        }
+    }
+
 
     public void setStatus(String status) {
         if (PaymentStatus.contains(status)) {
